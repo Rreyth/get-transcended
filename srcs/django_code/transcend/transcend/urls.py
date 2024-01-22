10472245be5f288f396beb35aaf6f32a88290f31
swotex <swotex21@gmail.com>
2024-01-22 18:48:37 +0100

    mise en place des docker (nginx, django, daphne) fonctionnel. projet django mise en place et premiere page creer/mise en place de l'architecture

--- srcs/django_code/transcend/transcend/urls.py
+++ srcs/django_code/transcend/transcend/urls.py
@@ -1,4 +1,4 @@
-"""project URL Configuration
+"""transcend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp import views
 
 urlpatterns = [
+    path('',  views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
