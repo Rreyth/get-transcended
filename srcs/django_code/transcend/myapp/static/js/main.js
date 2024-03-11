825acf1fdb11af056b7b1e4d5d3d3ce8456fcf3d
rvinour <rvinour@student.42angouleme.fr>
2024-03-11 14:02:58 +0100

    now if  will fetch

--- srcs/django_code/transcend/myapp/static/js/main.js
+++ srcs/django_code/transcend/myapp/static/js/main.js
@@ -1,43 +1,44 @@
-function loadPage(page) {
-    fetch(page)
-        .then(response => response.text())
-        .then(html => {
-				document.querySelector('body').innerHTML = html;
-		})
-			.catch(error => console.error('Error loading page:', error));
+async function loadPage(page) {
+	try {
+		var ftch = await fetch("/" + page);
+		var resp = await ftch.text();
+		document.querySelector('body').innerHTML = resp;
+	}
+	catch(error){
+		console.error('Error loading page:', error);
+	}
 }
 
 const router = async () => {
-    const routes = [
-        // { path: "/404", view: NotFound },
-        { path: "/", link:"static/html/home.html" },
-        { path: "/home", link:"static/html/home.html" },
-        { path: "/about", link: "static/html/about.html" },
-        { path: "/login", link: "static/html/login.html" },
-    ];
+	const routes = [
+		// { path: "/404", view: NotFound },
+		{ path: "/home", link:"static/html/home.html" },
+		{ path: "/about", link: "static/html/about.html" },
+		{ path: "/login", link: "static/html/login.html" },
+	];
 
-    const potentialMatches = routes.map(route => {
-        return {
-            route: route,
-            result: location.pathname == route.path,
-        };
-    });
+	const potentialMatches = routes.map(route => {
+		return {
+			route: route,
+			result: location.pathname.startsWith(route.path),
+		};
+	});
 
-    let match = potentialMatches.find(potentialMatch => potentialMatch.result !== false);
+	let match = potentialMatches.find(potentialMatch => potentialMatch.result !== false);
 
-    if (!match) {
-        match = {
-            route: routes[0],
-            result: [location.pathname]
-        };
-    }
-    loadPage(match.route.link);
+	if (!match) {
+		match = {
+			route: routes[0],
+			result: [location.pathname]
+		};
+	}
+	loadPage(match.route.link);
 };
 
 
 const navigateTo = url => {
 	history.pushState(null, null, url);
-    router();
+	router();
 };
 
 window.onload = router();
@@ -45,11 +46,11 @@ window.onload = router();
 window.addEventListener("popstate", router);
 
 document.addEventListener("DOMContentLoaded", () => {
-    document.body.addEventListener("click", e => {
+	document.body.addEventListener("click", e => {
 		if (!e.target.name){
 			e.preventDefault();
 			navigateTo(e.target.href);
 			console.log('Button clicked');
 		}
-    });
+	});
 });
