e8b154e138944e85f7ac98650600afeb9ae258cc
ltuffery <123221865+ltuffery@users.noreply.github.com>
2024-03-20 17:55:46 +0100

    add models

--- srcs/django_code/transcend/myapp/models.py
+++ srcs/django_code/transcend/myapp/models.py
@@ -9,3 +9,21 @@ class User(models.Model):
 	token = models.TextField()
 	createdAt = models.DateTimeField(auto_now_add=True, blank=True)
 	updatedAt = models.DateTimeField(auto_now_add=True, blank=True)
+
+class PrivateMessages(models.Model):
+	content = models.TextField()
+	sender_id = models.ForeignKey(on_delete=models.deletion.DO_NOTHING, to="User")
+	recever_id = models.ForeignKey(on_delete=models.deletion.DO_NOTHING, to="User")
+	created_at = models.DateTimeField(auto_now_add=True, blank=True, default=Now())
+
+class Channel(models.Model):
+	name = models.CharField(max_length=25)
+	owner_id = models.ForeignKey(on_delete=models.deletion.DO_NOTHING, to='User')
+	created_at = models.DateTimeField(auto_now_add=True, blank=True, default=Now())
+	updated_at = models.DateTimeField(auto_now_add=True, blank=True, default=Now())
+
+class Message(models.Model):
+	content = models.TextField()
+	channel_id = models.ForeignKey(on_delete=models.deletion.DO_NOTHING, to='channels')
+	user_id = models.ForeignKey(on_delete=models.deletion.DO_NOTHING, to='User')
+	created_at = models.DateTimeField(auto_now_add=True, blank=True, default=Now())
