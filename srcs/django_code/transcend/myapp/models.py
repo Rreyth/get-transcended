39147871913af83c36f72e3e2b74c699ad8f7ab5
ltuffery <123221865+ltuffery@users.noreply.github.com>
2024-03-31 21:21:51 +0200

    Ajout de migrations (#33)
    * change user migration
    
    * add env example
    
    * add migrations
    
    * add models
    
    * add unittests and fix models
    
    * update migration and add test (echec)
    
    * fix models and add tests
    
    * Add unique values in tables && add many-to-many relationship on channels - users
    
    * remove useless import

--- srcs/django_code/transcend/myapp/models.py
+++ srcs/django_code/transcend/myapp/models.py
@@ -1,4 +1,61 @@
 from django.db import models
+from django.db.models.functions import Now
 
 class User(models.Model):
-	pseudo = models.TextField()
+	pseudo = models.CharField(max_length=25, unique=True)
+	avatar = models.TextField(blank=True, default='')
+	email = models.CharField(max_length=255, unique=True)
+	password = models.TextField()
+	bot = models.BooleanField(null=True, default=False)
+	token = models.TextField(unique=True)
+	created_at = models.DateTimeField(auto_now_add=True, blank=True)
+	updated_at = models.DateTimeField(auto_now=True, blank=True)
+
+	def __str__(self) -> str:
+		return self.pseudo
+
+	class Meta:
+		db_table = "myapp_users"
+
+class PrivateMessage(models.Model):
+	content = models.TextField()
+	sender = models.ForeignKey(on_delete=models.deletion.DO_NOTHING, to="User", related_name="+")
+	recever = models.ForeignKey(on_delete=models.deletion.DO_NOTHING, to="User", related_name="+")
+	created_at = models.DateTimeField(auto_now_add=True, blank=True)
+
+	class Meta:
+		db_table = "myapp_private_messages" 
+
+class Channel(models.Model):
+	name = models.CharField(max_length=25, unique=True)
+	owner = models.ForeignKey(on_delete=models.deletion.DO_NOTHING, to='User', related_name='+')
+	created_at = models.DateTimeField(auto_now_add=True, blank=True)
+	updated_at = models.DateTimeField(auto_now=True, blank=True)
+
+	users = models.ManyToManyField(User, related_name="channel", related_query_name="channel", through='Membership')
+
+	def addUser(self, user: User) -> None:
+		self.users.add(user)
+	
+	def contains(self, user: User) -> bool:
+		return self.users.filter(pk=user.pk).exists() or user == self.owner
+
+	class Meta:
+		db_table = "myapp_channels"
+
+class Membership(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
+	joined_at = models.DateTimeField(auto_now=True, blank=True)
+
+	class Meta:
+		db_table = "myapp_channel_user"
+
+class Message(models.Model):
+	content = models.TextField()
+	channel = models.ForeignKey(on_delete=models.deletion.DO_NOTHING, to='Channel')
+	user = models.ForeignKey(on_delete=models.deletion.DO_NOTHING, to='User')
+	created_at = models.DateTimeField(auto_now_add=True, blank=True)
+
+	class Meta:
+		db_table = "myapp_messages"
