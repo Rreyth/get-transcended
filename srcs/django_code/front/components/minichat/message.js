cef00f3e43bbd8ab1701153b47d9902e8348783c
swotex <33070724+swotex@users.noreply.github.com>
2024-05-25 16:24:27 +0200

    [Issue-7]: implement chat (#58)
    * start implement new chat
    
    * continu new chat to component + start setting screen
    
    add users cards in js,
    start auto-cut + auto-scroll name (in user card),
    start popup setting screen (new component).
    need fix: auto-sroll (user card) is break after openning popup setting
    
    * save
    
    * update gitignore
    
    * add Middleware
    
    * fix middleware
    
    * change home redirect and api connexion (#68)
    
    * fix conflict
    
    * continu new chat to component + start setting screen
    
    add users cards in js,
    start auto-cut + auto-scroll name (in user card),
    start popup setting screen (new component).
    need fix: auto-sroll (user card) is break after openning popup setting
    
    * save
    
    * fix error conflict fix
    
    * send message by channel_layer.group
    
    * change structure files
    
    * save message in db
    
    * change -> startapp chat
    
    * add api path for list dm
    
    * fix api function
    
    * add cache system
    
    * update dynamique chat
    
    * add protect friends list empty
    
    * fix messages == null
    
    * add friend select
    
    * fix websocket sender (sender no receve message)
    
    * fix message sender username
    
    * start new css style
    
    * add setWebSocket function
    
    * message design and fix scrool bottom conv
    
    * remove comments
    
    * fix #72
    
    ---------
    
    Co-authored-by: ltuffery <123221865+ltuffery@users.noreply.github.com>

--- srcs/django_code/front/components/minichat/message.js
+++ srcs/django_code/front/components/minichat/message.js
@@ -1,23 +1,26 @@
 import { Component } from "../../js/component.js";
+import { user } from "../../js/helpers.js";
 
-export class Message extends Component
-{
+export class Message extends Component {
 
-    static getName()
-    {
+    static getName() {
         return "message"
     }
 
-    connectedCallback()
-    {
+    async connectedCallback() {
         super.connectedCallback()
 
-        const who = this.getAttribute('who') == "reciever" ? "recieveMsg" : "sendMsg"
+        const align = this.getAttribute('who') == (await user()).username ? "align-items-end" : "align-items-start"
+        const userStyle = this.getAttribute('who') == (await user()).username ? "border-radius: 0.625rem 0.625rem 0rem 0.625rem; background: #AAA;" : "border-radius: 0.625rem 0.625rem 0.625rem 0rem; background: #5F6FF9;"
 
         this.innerHTML = `
-        <div class="msgContainer">
-            <div class="${who}">
-                <p>${this.getAttribute('content')}</p>
+        <div class="d-flex flex-column ${align} align-self-stretch" style="gap: 0.3125rem;">
+            <span>${this.getAttribute('who')}</span>
+            <div class="d-flex flex-column text-wrap justify-content-end align-items-start" style="max-width: 9.375rem; padding: 0.3125rem; ${userStyle}; overflow-wrap: break-word;">
+                <p class="align-self-stretch">
+                    ${this.getAttribute('content')}
+                </p>
+                <span class="align-self-stretch" style="font-size: 0.5rem;">${this.getAttribute('date')}</span>
             </div>
         </div>
         `
