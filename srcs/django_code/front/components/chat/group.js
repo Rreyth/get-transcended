af957a5d60f69ec1d3f2de6e6b034cf220001b90
ltuffery <123221865+ltuffery@users.noreply.github.com>
2024-06-11 09:15:54 +0200

    add goup component

--- srcs/django_code/front/components/chat/group.js
+++ srcs/django_code/front/components/chat/group.js
@@ -7,5 +7,14 @@ export class Group extends Component
         return "group"
     }
 
-    
+    connectedCallback()
+    {
+        this.classList.add("list-group-item", "list-group-item-action")
+
+        this.innerHTML = /* html */`
+            <div class="d-flex align-items-center gap-2" id="group-card">
+                <span>${this.getAttribute('name')}</span>
+            </div>
+        `
+    }
 }
