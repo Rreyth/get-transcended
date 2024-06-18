aa6392f30833249359684e4a6569b54640e4c332
Jukaro <jeremi1005@outlook.fr>
2024-06-18 15:02:12 +0200

    indentation fixed

--- srcs/django_code/transcend/transcend/settings.py
+++ srcs/django_code/transcend/transcend/settings.py
@@ -41,7 +41,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'nginx', address]
 # Application definition
 
 INSTALLED_APPS = [
-	'daphne',
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
