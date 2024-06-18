c216265d4a519bbc320e53fd9c13867818046b6c
Jukaro <45951179+Jukaro@users.noreply.github.com>
2024-06-18 18:13:39 +0200

    add StatReloader (#170)
    * add daphne to django installed apps to use runserver as daphne and get StatReloader to watch for file changes and reload the server
    
    * indentation fixed

--- srcs/requirements/Daphne/utils/start.sh
+++ srcs/requirements/Daphne/utils/start.sh
@@ -2,4 +2,4 @@ sleep 1
 
 python3 manage.py migrate
 python manage.py loaddata users/seed/AI_Account.json chat/seed/Main_Chat.json
-daphne transcend.asgi:application -b 0.0.0.0 -p 8001
+python manage.py runserver 0.0.0.0:8001
