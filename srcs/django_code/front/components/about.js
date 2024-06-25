33e2980c9b3618ed9da3f04577631b8d9c8f516f
Rreyth <dhaussy.terry99@gmail.com>
2024-06-25 21:36:39 +0200

    update about

--- srcs/django_code/front/components/about.js
+++ srcs/django_code/front/components/about.js
@@ -89,8 +89,8 @@ const content = /*html*/`
 				<p class="mt-1">${ await translate("about.42_stud") }</p>
 				<div class="d-flex justify-content-evenly w-100 mb-3">
 					<a href="https://github.com/Rreyth" class='bx bxl-github bx-sm text-decoration-none text-reset'></a>
-					<a href="" class='bx bxl-linkedin-square bx-sm text-decoration-none text-reset'></a>
-					<a href="mailto:" class='bx bx-mail-send bx-sm text-decoration-none text-reset'></a>
+					<a href="https://fr.linkedin.com/in/terry-dhaussy-3a0617158" class='bx bxl-linkedin-square bx-sm text-decoration-none text-reset'></a>
+					<a href="mailto:tdhaussy@student.42angouleme.fr" class='bx bx-mail-send bx-sm text-decoration-none text-reset'></a>
 				</div>
 			</div>
 
@@ -105,6 +105,19 @@ const content = /*html*/`
 					<a href="https://github.com/swotex" class='bx bxl-github bx-sm text-decoration-none text-reset'></a>
 					<a href="https://fr.linkedin.com/in/nicolas-jegat-b13440179" class='bx bxl-linkedin-square bx-sm text-decoration-none text-reset'></a>
 					<a href="mailto:nicojegat21@gmail.com" class='bx bx-mail-send bx-sm text-decoration-none text-reset'></a>
+				</div>
+			</div>
+
+			<div class="col d-flex flex-column align-items-center mx-1 rounded-3 card border-light">
+				<div>
+					<img class="rounded-4 mt-3" src="https://cdn.intra.42.fr/users/556a364b2cdfb47ada125d9667564004/jfarkas.jpg" style="width: 7em; height: 7em; object-fit: cover; object-position: center;"/>
+				</div>
+				<span class="mt-2" style="font-size: 1.8em;">jfarkas</span>
+				<span class="align-self-start mt-3" style="font-size: 1.3em;">${ await translate("about.about") } :</span>
+				<p class="mt-1">${ await translate("about.42_stud") }</p>
+				<div class="d-flex justify-content-evenly w-100 mb-3">
+					<a href="https://github.com/Jukaro" class='bx bxl-github bx-sm text-decoration-none text-reset'></a>
+					<a href="mailto:jfarkas@student.42angouleme.fr" class='bx bx-mail-send bx-sm text-decoration-none text-reset'></a>
 				</div>
 			</div>
 
