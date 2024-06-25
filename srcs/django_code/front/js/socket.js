8cabe1620eccf79315e83d0e552f162fd0fdce99
Rreyth <dhaussy.terry99@gmail.com>
2024-06-25 18:44:37 +0200

    fix ws init and reset

--- srcs/django_code/front/js/socket.js
+++ srcs/django_code/front/js/socket.js
@@ -2,18 +2,38 @@ import { user_token, token_checker } from "./helpers.js"
 
 export class Socket
 {
-    static routes = {}
+    static routes = {};
+    static save = {};
+	static isSet = false;
 
     static async set(path, callback)
     {
-		await token_checker()
-        const token = await user_token()
+		this.save[path] = callback;
+    }
+
+	static async run() {
+		await token_checker();
+		const token = await user_token();
+		if (!token)
+			this.cleanRoutes();
+		if (this.isSet)
+			return;
 
         if (token)
         {
-            this.routes[path] = new WebSocket(`wss://${window.location.host}/ws${path}?token=${token}`)
-            this.routes[path].onmessage = callback
+			for (const [path, callback] of Object.entries(this.save)) {
+            	this.routes[path] = new WebSocket(`wss://${window.location.host}/ws${path}?token=${token}`);
+            	this.routes[path].onmessage = callback;
+			}
+			this.isSet = true;
         }
-    }
+	}
 
+	static cleanRoutes() {
+		this.isSet = false;
+		for (const ws of Object.values(this.routes)) {
+			ws.close();
+		}
+		this.routes = {};
+	}
 }
