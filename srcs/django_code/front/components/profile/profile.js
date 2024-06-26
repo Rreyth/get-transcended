b02b71bc7d37aa8315c668cea8f13832ae11587f
Rreyth <dhaussy.terry99@gmail.com>
2024-06-26 10:58:26 +0200

    profile winrate fix

--- srcs/django_code/front/components/profile/profile.js
+++ srcs/django_code/front/components/profile/profile.js
@@ -22,7 +22,8 @@ export class Profile extends HTMLElement {
 			connected: data.online,
 			wins: data.wins,
 			games: data.games,
-			winrate: data.games ? data.wins * 100 / data.games : 0,
+			winrate: data.winrate,
+			// winrate: data.games ? data.wins * 100 / data.games : 0,
 			target_username: user,
 			target_id: data.id,
 		}
