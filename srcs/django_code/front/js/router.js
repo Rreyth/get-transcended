ba92889c6080b980320855a073f8d12f9b090c76
Jukaro <jeremi1005@outlook.fr>
2024-06-26 22:34:54 +0200

    add history to the SPA

--- srcs/django_code/front/js/router.js
+++ srcs/django_code/front/js/router.js
@@ -3,11 +3,11 @@ import { user, token_checker } from "./helpers.js";
 import { Socket } from "./socket.js";
 
 export class Router {
-  static async push(pathname) {
+  static async push(pathname, dontPush = false) {
 
     await token_checker();
     const userInfo = await user();
-    
+
     const urlParams = new URLSearchParams(window.location.search);
     const code = urlParams.get('code');
 
@@ -16,14 +16,15 @@ export class Router {
     }
     else if (userInfo && (pathname == "/login" || pathname == "login"))
       pathname = "/";
-    
+
     Thread.clearAll();
     Socket.run();
 
     const url = new URL(pathname, window.location.origin);
     const response = await fetch(url, { headers: { "X-Source": "SPA" } });
     const text = await response.text();
-    window.history.pushState(undefined, "", url);
+    if (!dontPush)
+      window.history.pushState(null, "", pathname);
     let link = url.pathname;
     let title = link.substring(url.pathname.lastIndexOf('/') + 1);
     if (title.length == 0) {
@@ -37,6 +38,10 @@ export class Router {
     document.querySelector("#content").innerHTML = text;
     window.dispatchEvent(new Event("refreshNavbar"));
   }
+}
+
+window.onpopstate = function() {
+    Router.push(window.location.pathname, true);
 }
 
 window.addEventListener("load", () => {
